2f3c09e390f3f91070486df3a1ac6677ba0b6492
Jessie Doherty <zhuzhu930@yahoo.com>
2022-07-17 20:30:17 -0500

    editing projects.js

--- jd-react-portfolio/src/components/pages/Projects.js
+++ jd-react-portfolio/src/components/pages/Projects.js
@@ -1,6 +1,60 @@
 import React from 'react';
+import SoundRiver from '../../images/SoundRiver.png';
+import GadgetGetter from '../../images/GadgetGetter.png';
+import Trivia from '../../images/triviaOfImpressionism.png';
+import NoteTaker from '../../images/ExpressNoteTaker.png';
 
 export default function Projects() {
+
+  // Set up list item array: 
+  const projectList = [
+    {
+      name: "SoundRiver Music App",
+      repo: "https://github.com/zhuzhu930/SoundRiver-Proj1",
+      webpage: "https://zhuzhu930.github.io/SoundRiver-Proj1/",
+      screenshot: SoundRiver
+    },
+    {
+      name: "GadgetGetter App",
+      repo: "https://github.com/zhuzhu930/GadgetGetter-Project2",
+      webpage:"https://gadget-getter.herokuapp.com/login",
+      screenshot: GadgetGetter
+    },
+    {
+      name: "Trivia of Impressionism",
+      repo: "https://github.com/zhuzhu930/hw4-CodeQuiz-JD",
+      webpage:"https://zhuzhu930.github.io/hw4-CodeQuiz-JD/",
+      screenshot: Trivia
+    },
+    {
+      name: "Express.js Note Taker",
+      repo: "https://github.com/zhuzhu930/hw11_ExpessJSNoteTaker_JD",
+      webpage: "https://obscure-lake-98693.herokuapp.com/",
+      screenshot: NoteTaker
+    }
+  ];
+
+  // Iterate list items: 
+  const ProjectItem = ({list}) => (
+    <ul className="repolist">
+      {list.map(item =>
+        <li>
+            <p>{item.name}</p>
+            <a
+              target="_blank"
+              href={item.url}
+              >Repo</a>
+            <a
+              target="_blank"
+              href="https://zhuzhu930.github.io/SoundRiver-Proj1/"
+              >Webpage</a>
+            <a target="_blank" href="assets/images/SoundRiver.png"
+              >Screenshot</a>
+          </li>
+      )}
+    </ul>  
+  );
+
   return (
     <div className="card">
         <h2>Selected Repositories</h2>
