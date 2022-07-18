8250166f255e0ddf01e2468254347ee8d6daecf3
Jessie Doherty <zhuzhu930@yahoo.com>
2022-07-17 21:01:45 -0500

    fixing styles

--- jd-react-portfolio/src/components/pages/Contact.js
+++ jd-react-portfolio/src/components/pages/Contact.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
 export default function Contact() {
+
+  const handleClick = () => {
+    window.alert(`Your message is saved, I will get back to you ASAP.`);
+  };
+
   return (
     <div className="card">
       <h3>Send me a message: </h3>
       <div className="form">
           <div className="mb-3">
         <label for="exampleFormControlInput1" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+        <input type="email" className="form-control" id="emailInput" placeholder="name@example.com"/>
       </div>
 
       <div className="mb-3">
         <label for="exampleFormControlTextarea1" className="form-label">Your message: </label>
-        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        <textarea className="form-control" id="messageInput" rows="3"></textarea>
       </div>
-      <button type="button" className="btn btn-primary">Submit</button>
+      <button type="button" className="btn btn-primary" onClick={handleClick}>Submit</button>
       </div>  
     </div>)
   }
--- jd-react-portfolio/src/components/pages/Projects.js
+++ jd-react-portfolio/src/components/pages/Projects.js
@@ -42,13 +42,13 @@ export default function Projects() {
             <p>{item.name}</p>
             <a
               target="_blank"
-              href={item.url}
+              href={item.repo}
               >Repo</a>
             <a
               target="_blank"
-              href="https://zhuzhu930.github.io/SoundRiver-Proj1/"
+              href={item.webpage}
               >Webpage</a>
-            <a target="_blank" href="assets/images/SoundRiver.png"
+            <a target="_blank" href={item.screenshot}
               >Screenshot</a>
           </li>
       )}
@@ -62,56 +62,7 @@ export default function Projects() {
           Please feel free to take a look at some of the sites I've designed or
           participated in designing:
         </p>
-        <ul className="repolist">
-          <li>
-            <p>SoundRiver Music App</p>
-            <a
-              target="_blank"
-              href="https://github.com/zhuzhu930/SoundRiver-Proj1"
-              >Repo</a>
-            <a
-              target="_blank"
-              href="https://zhuzhu930.github.io/SoundRiver-Proj1/"
-              >Webpage</a>
-            <a target="_blank" href="assets/images/SoundRiver.png"
-              >Screenshot</a>
-          </li>
-          <li>
-            <p>GadgetGetter App</p>
-            <a
-              target="_blank"
-              href="https://github.com/zhuzhu930/GadgetGetter-Project2"
-              >Repo</a>
-            <a target="_blank" href="https://gadget-getter.herokuapp.com/login"
-              >Webpage</a>
-            <a target="_blank" href="assets/images/GadgetGetter.png"
-              >Screenshot</a>
-          </li>
-          <li>
-            <p>Trivia of Impressionism</p>
-            <a
-              target="_blank"
-              href="https://github.com/zhuzhu930/hw4-CodeQuiz-JD"
-              >Repo</a>
-            <a
-              target="_blank"
-              href="https://zhuzhu930.github.io/hw4-CodeQuiz-JD/"
-              >Webpage</a>
-            <a target="_blank" href="assets/images/triviaOfImpressionism.png"
-              >Screenshot</a>
-          </li>
-          <li>
-            <p>Express.js Note Taker</p>
-            <a
-              target="_blank"
-              href="https://github.com/zhuzhu930/hw11_ExpessJSNoteTaker_JD"
-              >Repo</a>
-            <a target="_blank" href="https://obscure-lake-98693.herokuapp.com/"
-              >Webpage</a>
-            <a target="_blank" href="assets/images/ExpressNoteTaker.png"
-              >Screenshot</a>
-          </li>
-        </ul>
+        <ProjectItem list={projectList}/>
       </div>
   );
 }
